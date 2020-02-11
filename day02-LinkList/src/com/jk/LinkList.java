f6a77da86f7dc444fc73a5c73b59815f516ad399
even <even@evendeMacBook-Pro.local>
2020-02-12 03:10:40 +0800

    day02

---
+++ day02-LinkList/src/com/jk/LinkList.java
@@ -0,0 +1,10 @@
+package com.jk;
+
+public class LinkList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ day02-LinkList/src/com/jk/Main.java
@@ -0,0 +1,7 @@
+package com.jk;
+
+public class Main {
+	public static void main(String[] args) {
+			
+	}
+}
